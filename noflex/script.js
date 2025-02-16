8e746e54c15724b7e7e1f86fce65f5a6297cc604
Abuel Bayangan <32684054+abuelthinks@users.noreply.github.com>
2025-02-17 04:47:56 +0800

    added sections: trending, reasons, faq, cta and footer

---
+++ noflex/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {    
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: "5",
+        spaceBetween: 15,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
